29f502c352b065b54167a19e4774407c3e31c5c8
Nick Bird <bird.nick.95@gmail.com>
2020-12-23 16:55:07 -0800

    Finished Objective 2 Labs 1, 2, and 3

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,9 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Nick Bird";
+    int age = 25;
+    String state = "Colorado";
+    System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+
+  }
+}

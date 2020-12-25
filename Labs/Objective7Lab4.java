21fad79114cb6d2ccc03e482feea899e74e973fe
Nick Bird <bird.nick.95@gmail.com>
2020-12-25 14:59:47 -0800

    Finished Objective 7: Labs 1, 2, 3, 4, and 5

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,12 @@
+public class Objective7Lab4 {
+  public static void main(String[] args) {
+    int count = 0;
+    int sum = 0;
+
+    while(count < 20) {
+      count = count +1;
+      sum = count + sum;
+      }
+    System.out.println(sum);
+  }
+}

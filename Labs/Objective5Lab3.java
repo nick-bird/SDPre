cffbd24441cecb7d9d87b540b6cb9ae7fbc67d65
Nick Bird <bird.nick.95@gmail.com>
2020-12-24 17:59:07 -0800

    Finished Objective 5: Labs 1, 2, 3, and 4

---
+++ Labs/Objective5Lab3.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+//Could not compile without adding above line 
+public class Objective5Lab3 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int userNum;
+
+    System.out.println("Please enter a number: ");
+    userNum = scanner.nextInt();
+
+    if(userNum > 0) {
+      System.out.println("The number is positive.");
+    }
+    else if(userNum < 0) {
+      System.out.println("The number is negative.");
+    }
+    else{
+      System.out.println("The number is 0.");
+    }
+    scanner.close();
+  }
+}
